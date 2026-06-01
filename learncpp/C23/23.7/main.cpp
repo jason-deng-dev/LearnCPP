a071542a9d76c2110bad909d46eb9ff7e864382d
jason-deng <jasondeng.dev@gmail.com>
2026-06-01 15:06:04 +0800

    23.7

---
+++ learncpp/C23/23.7/main.cpp
@@ -0,0 +1,82 @@
+#include <algorithm> // for std::copy()
+#include <cassert>   // for assert()
+#include <cstddef>
+#include <initializer_list> // for std::initializer_list
+#include <iostream>
+
+class IntArray
+{
+private:
+	int m_length{};
+	int* m_data{};
+
+public:
+	IntArray() = default;
+
+	IntArray(int length)
+	    : m_length{length}
+	    , m_data{new int[static_cast<std::size_t>(length)]{}}
+	{
+	}
+
+	IntArray(std::initializer_list<int>
+	             list) // allow IntArray to be initialized via list initialization
+	    : IntArray(static_cast<int>(
+	          list.size())) // use delegating constructor to set up initial array
+	{
+		// Now initialize our array from the list
+		std::copy(list.begin(), list.end(), m_data);
+	}
+
+	~IntArray() { delete[] m_data; }
+
+	//	IntArray(const IntArray&) = delete; // to avoid shallow copies
+	//	IntArray& operator=(const IntArray& list) = delete; // to avoid shallow
+	//copies
+
+	int& operator[](int index)
+	{
+		assert(index >= 0 && index < m_length);
+		return m_data[index];
+	}
+
+	int getLength() const { return m_length; }
+
+	IntArray& operator=(std::initializer_list<int> list)
+	{
+    // only need to reallocate if different size
+		if (m_length != static_cast<int>(list.size()))
+		{
+			delete[] m_data;
+			m_length = static_cast<int>(list.size());
+			m_data = new int[static_cast<std::size_t>(m_length)]{};
+		}
+
+		// for (std::size_t i{}; i < list.size(); ++i)
+		// {
+		// 	m_data[i] = list.begin()[i];
+		// }
+		// can use std::copy
+		std::copy(list.begin(), list.end(), m_data);
+
+		return *this;
+	}
+};
+
+int main()
+{
+	IntArray array{5, 4, 3, 2, 1}; // initializer list
+	for (int count{0}; count < array.getLength(); ++count)
+		std::cout << array[count] << ' ';
+
+	std::cout << '\n';
+
+	array = {1, 3, 5, 7, 9, 11};
+
+	for (int count{0}; count < array.getLength(); ++count)
+		std::cout << array[count] << ' ';
+
+	std::cout << '\n';
+
+	return 0;
+}
